742ae5cce10cccdbdf00d5b3223a2696e3ad4c1f
Thomas Howe <howethomas@users.noreply.github.com>
2024-08-15 20:01:22 -0400

    Adds pacakage management.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="vcon",
+    version="0.3.0",
+    author="Thomas McCarthy-Howe",
+    author_email="ghostofbasho@gmail.com",
+    description="A package for working with vCon containers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vcon-dev/vcon-lib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
